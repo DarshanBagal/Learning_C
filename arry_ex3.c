a8986e0bcf93fde09dbaa73701d33f21c1751282
DarshanBagal <darshanbagal200411@gmail.com>
2026-02-15 23:47:15 +0530

    Create arry_ex3.c

---
+++ arry_ex3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int marks[5]={78,67,45,90,79};
+	int i;
+	
+	printf("Arrys elements");
+	for(i=4;i>=0;i--){
+		printf("\n %d",marks[i]);
+	}
+	
+	
+	
+	return 0;
+}
